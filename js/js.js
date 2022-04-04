60708b280c30b8a00b892637f0f9f0311cf5f4a3
Lina <eng.lina870@gmail.com>
2022-04-04 08:55:40 +0200

    first commit

---
+++ js/js.js
@@ -0,0 +1,33 @@
+
+
+document.getElementById("btn-1").addEventListener("click",addRow );
+
+function addRow()
+{
+    // get input values
+    var fname = document.getElementById('fname').value;
+     var lname = document.getElementById('lname').value;
+      var email = document.getElementById('email').value;
+      
+      // get the html table
+      // 0 = the first table
+      var table = document.getElementsByTagName('table')[0];
+      
+      // add new empty row to the table
+      // 0 = in the top 
+      // table.rows.length = the end
+      // table.rows.length/2+1 = the center
+      var newRow = table.insertRow(table.rows.length);
+      
+      // add cells to the row
+      var cel1 = newRow.insertCell(0);
+      var cel2 = newRow.insertCell(1);
+      var cel3 = newRow.insertCell(2);
+      
+      // add values to the cells
+      cel1.innerHTML = fname;
+      cel2.innerHTML = lname;
+      cel3.innerHTML = email;
+    
+
+}
